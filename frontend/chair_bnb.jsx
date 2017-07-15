661ae79219601f4a8782981cefeb235e1f5ead01
Anastassia Bobokalonova <email.anastassia@gmail.com>
2017-07-15 16:45:13 -0700

    Create session form component

--- frontend/chair_bnb.jsx
+++ frontend/chair_bnb.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { signup, logout, login } from './util/session_api_util.js';
+import { signup, logout, login } from './actions/session_actions';
 import Root from './components/root';
 import configureStore from './store/store';
 
@@ -9,6 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   window.getState = store.getState;
   window.dispatch = store.dispatch;
+  window.login = login;
 
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={ store }/>, root);
---
+++ frontend/components/session_form/session_form_container.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Link, withRouter } from 'react-router-dom';
+
+class SessionForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: ''
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.loggedIn) {
+      this.props.history.push('/');
+    }
+  }
+
+  update(field) {
+
+
+  }
+
+  handleSubmit(e) {
+
+
+  }
+
+  navLink() {
+
+
+  }
+
+  renderErrors() {
+
+
+  }
+
+  render() {
+
+
+  }
+}
+
+export default withRouter(SessionForm);
