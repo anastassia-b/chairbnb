68f30a5d2ba6fedc505c0615bb260a5aa6302b13
Anastassia Bobokalonova <email.anastassia@gmail.com>
2017-07-15 15:48:16 -0700

    Add greeting component

--- frontend/chair_bnb.jsx
+++ frontend/chair_bnb.jsx
@@ -5,14 +5,11 @@ import Root from './components/root';
 import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
-  let store;
-  if (window.currentUser) {
-    const preloadedState = { session: { currentUser: window.currentUser } };
-    store = configureStore(preloadedState);
-    delete window.currentUser;
-  } else {
-    store = configureStore();
-  }
+  const store = configureStore();
+
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+
   const root = document.getElementById('root');
-  ReactDOM.render(<Root store={store} />, root);
+  ReactDOM.render(<Root store={ store }/>, root);
 });
--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-
+import GreetingContainer from './greeting/greeting_container';
 
 const App = () => (
   <div>
     <header>
         <h1>Chair BnB!</h1>
+        <GreetingContainer />
     </header>
   </div>
 );
