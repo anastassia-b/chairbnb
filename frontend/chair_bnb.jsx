a475fb13b7919e790eb3d35eea32d78610d5f5ac
Anastassia Bobokalonova <email.anastassia@gmail.com>
2017-07-14 19:40:18 -0700

    Create store

--- frontend/chair_bnb.jsx
+++ frontend/chair_bnb.jsx
@@ -2,11 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import { signup, logout, login } from './util/session_api_util.js';
 
-document.addEventListener('DOMContentLoaded', () => {
-  // window.login = login;
-  // window.logout = logout;
-  // window.signup = signup;
+import configureStore from './store/store';
 
+document.addEventListener('DOMContentLoaded', () => {
+  let store;
+  if (window.currentUser) {
+    const preloadedState = { session: { currentUser: window.currentUser } };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Welcome to ChairBnB!</h1>, root);
 });
