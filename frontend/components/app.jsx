1cbaa532b930db851f5096bee64ca153f65d8456
Anastassia Bobokalonova <email.anastassia@gmail.com>
2017-07-17 11:31:14 -0700

    Add session CSS styling

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { Route, HashRouter } from 'react-router-dom';
+import { Route, HashRouter, Link } from 'react-router-dom';
 import GreetingContainer from './greeting/greeting_container';
 import SessionFormContainer from './session_form/session_form_container';
 
 const App = () => (
   <div>
     <header>
+      <Link to="/" className="header-link">
         <h1>Chair BnB!</h1>
-        <GreetingContainer />
+      </Link>
+      <GreetingContainer />
     </header>
     <Route path="/login" component={SessionFormContainer} />
     <Route path="/signup" component={SessionFormContainer} />
