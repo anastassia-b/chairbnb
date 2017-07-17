21589970824f55983f33477dd5373dba22e203b6
Anastassia Bobokalonova <email.anastassia@gmail.com>
2017-07-17 12:07:27 -0700

    Bootstrap current user

--- frontend/chair_bnb.jsx
+++ frontend/chair_bnb.jsx
@@ -5,11 +5,18 @@ import Root from './components/root';
 import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const store = configureStore();
-
   window.getState = store.getState;
   window.dispatch = store.dispatch;
   window.login = login;
+
+  let store;
+  if (window.currentUser) {
+    const preloadedState = { session: { currentUser: window.currentUser } };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
 
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={ store }/>, root);
